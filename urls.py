18c0b7d302368cbe99c698db076e704fffc86c2a
Md. Taufiqur Rahman <56925314+MdTaufiqurRahman@users.noreply.github.com>
2020-09-15 18:59:01 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,7 @@
 
 from django.urls import path
-from .views import BooksListView, BooksDetailView, BookCheckoutView, paymentComplete, SearchResultsListView
-
+from .views import SignUpView
 
 urlpatterns = [
-    path('', BooksListView.as_view(), name = 'list'),
-    path('<int:pk>/', BooksDetailView.as_view(), name = 'detail'),
-    path('<int:pk>/checkout/', BookCheckoutView.as_view(), name = 'checkout'),
-    path('complete/', paymentComplete, name = 'complete'),
-    path('search/', SearchResultsListView.as_view(), name = 'search_results'),
+    path('accounts/', SignUpView.as_view(), name = 'signup'),
 ]
