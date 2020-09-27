e8463c90c95f65833850751ab7d1fdbde546e299
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2020-09-27 08:23:15 +0600

    Add files via upload

--- models.py
+++ models.py
@@ -1,24 +1,56 @@
-
 from django.db import models
-from django.urls import reverse
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils import timezone
 
-class Book(models.Model):
-    title  = models.CharField(max_length = 200)
-    author = models.CharField(max_length = 200)
-    description = models.CharField(max_length = 500, default=None)
-    price = models.FloatField(null=True, blank=True)
-    image_url = models.CharField(max_length = 2083, default=False)
-    follow_author = models.CharField(max_length=2083, blank=True)  
-    book_available = models.BooleanField(default=False)
+
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_post_author')
+    blog_title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField(verbose_name="What is on your mind?")
+    
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
-        return self.title
+        return self.blog_title
 
 
-class Order(models.Model):
-	product = models.ForeignKey(Book, max_length=200, null=True, blank=True, on_delete = models.SET_NULL)
-	created =  models.DateTimeField(auto_now_add=True) 
 
-	def __str__(self):
-		return self.product.title
-    
+class BlogComment(models.Model):
+    blog_post=models.ForeignKey(BlogPost,on_delete=models.CASCADE,related_name='blog_post_comment')
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_user_comment')
+    blog_comment=models.TextField()
+    blog_comment_date=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=('-blog_comment_date',)
+
+
+    def __str__(self):
+        return self.blog_comment
+
+
+
+
+class Likes(models.Model):
+    blog=models.ForeignKey(BlogPost,on_delete=models.CASCADE,related_name='liked_blog')
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='liked_user')
+
+
+
+    def __str__(self):
+        return self.user+ " likes " + self.blog
--- urls.py
+++ urls.py
@@ -1,12 +1,26 @@
-
+from . import views
 from django.urls import path
-from .views import BooksListView, BooksDetailView, BookCheckoutView, paymentComplete, SearchResultsListView
 
+app_name='blog'
 
 urlpatterns = [
-    path('', BooksListView.as_view(), name = 'list'),
-    path('<int:pk>/', BooksDetailView.as_view(), name = 'detail'),
-    path('<int:pk>/checkout/', BookCheckoutView.as_view(), name = 'checkout'),
-    path('complete/', paymentComplete, name = 'complete'),
-    path('search/', SearchResultsListView.as_view(), name = 'search_results'),
+    path('blog/', views.BlogList.as_view(), name='blog_list'),
+    path('blog/write/', views.CreateBlog.as_view(), name='create_blog'),
+    path('blog/details/<slug:slug>', views.Blog_Details, name='blog_detail'),
+    path('blog/liked/<pk>/', views.liked, name='liked_post'),
+    path('blog/unliked/<pk>/', views.unliked, name='unliked_post'),
 ]
+
+"""At first i imported 'path' module from django.url library which we will use for url routing, 
+then in urlpatterns section im telling django at which location or url which webpage should work.
+
+For BlogList.as_view() i used 'blog/'
+it tells django that on the very first page work as per the BlogList class
+and then i choose a reference name for this url as 'blog_list' and mentioned it inside name.
+
+Next is CreateBlog which will be loaded after the BlogList.
+Then Blogdetail,Liked and Unliked will be loaded as per click.
+on BlogDetail slug is passed as slug line of the post.
+And on liked and unliked pk(primary key) is passed.
+
+"""
