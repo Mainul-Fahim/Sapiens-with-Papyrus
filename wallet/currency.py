104c9943546a2e695dee896cf4aaaf7e64211f36
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2020-09-27 13:23:54 +0600

    Add files via upload

--- wallet/currency.py
+++ wallet/currency.py
@@ -17,13 +17,13 @@ class ConcurrentDepositTestCase(WalletTestCase):
                         pk=self.wallet.id)
                 wallet.deposit(DEPOSIT)
 
-                # We simulate a long transaction so that
-                # when the other thread comes in, this
-                # thread still holds the lock.
+                """Simulating a long transaction so that
+                 when the other thread comes in, this
+                 thread still holds the lock."""
                 time.sleep(1)
 
-        # We run the two threads to simulate two
-        # transactions running simultaneously.
+        """Running the two threads to simulate two
+         transactions running simultaneously."""
         t1 = threading.Thread(target=deposit_thread)
         t2 = threading.Thread(target=deposit_thread)
         t1.start()
@@ -31,17 +31,17 @@ class ConcurrentDepositTestCase(WalletTestCase):
         t1.join()
         t2.join()
 
-        # We retrieve a new wallet object since what we
-        # currently have is now stale.
+        """Retrieving a new wallet object since what we
+         currently have is now stale."""
         wallet = Wallet.objects.get(pk=self.wallet.id)
 
-        # We check that the current_balance wasn't
-        # overwritten by the other transaction. In short,
-        # we should get the correct deposited total amount.
+        """ Checking that the current_balance wasn't
+         overwritten by the other transaction. In short,
+         we should get the correct deposited total amount."""
         self.assertEqual(wallet.current_balance, DEPOSIT * 2)
 
-        # We then check that we have two transactions. Each
-        # having the deposited amount.
+        """Checking that we have two transactions. Each
+         having the deposited amount."""
         self.assertEqual(wallet.transaction_set.count(), 2)
 
     def test_withdraw(self):
@@ -56,9 +56,9 @@ class ConcurrentDepositTestCase(WalletTestCase):
                         pk=self.wallet.id)
                 wallet.withdraw(WITHDRAW)
 
-                # We simulate a long transaction so that
-                # when the other thread comes in, this
-                # thread still holds the lock.
+                 """ Simulating a long transaction so that
+                 when the other thread comes in, this
+                thread still holds the lock."""
                 time.sleep(1)
 
         t1 = threading.Thread(target=withdraw_thread)
@@ -70,18 +70,18 @@ class ConcurrentDepositTestCase(WalletTestCase):
 
         wallet = Wallet.objects.get(pk=self.wallet.id)
 
-        # Assert that both transactions were able to
-        # succeed.
+        """ Assert that both transactions were able to
+         succeed."""
         self.assertEqual(wallet.current_balance,
                 INITIAL_BALANCE - (WITHDRAW * 2))
 
-        # We also check that the number of transactions
-        # created only equates to the number of
-        # transactions successful + the initial transaction.
+        """Checking that the number of transactions
+         created only equates to the number of
+         transactions successful + the initial transaction."""
         self.assertEqual(wallet.transaction_set.count(), 3)
 
     def test_multiple_withdraw_insufficient_balance(self):
-        """We're going to test two concurrent withdraw
+        """Testing two concurrent withdraw
         transactions happening in parallel where one would
         end-up with an insufficient balance."""
         INITIAL_BALANCE = 199
@@ -94,9 +94,9 @@ class ConcurrentDepositTestCase(WalletTestCase):
                         pk=self.wallet.id)
                 wallet.withdraw(WITHDRAW)
 
-                # We simulate a long transaction so that
-                # when the other thread comes in, this
-                # thread still holds the lock.
+                """ Simulating a long transaction so that
+                 when the other thread comes in, this
+                thread still holds the lock."""
                 time.sleep(1)
 
         t1 = threading.Thread(target=withdraw_thread)
@@ -108,22 +108,22 @@ class ConcurrentDepositTestCase(WalletTestCase):
 
         wallet = Wallet.objects.get(pk=self.wallet.id)
 
-        # Assert that the only the first transaction was
-        # able to succeed by checking that only one
-        # transaction's value was applied.
+        """ Asserting that the only the first transaction was
+         able to succeed by checking that only one
+         transaction's value was applied."""
         self.assertEqual(wallet.current_balance,
                 INITIAL_BALANCE - (WITHDRAW * 1))
 
-        # We also check that the number of transactions
-        # created only equates to the number of
-        # transactions successful + the initial transaction.
+        """ Checking that the number of transactions
+         created only equates to the number of
+         transactions successful + the initial transaction."""
         self.assertEqual(wallet.transaction_set.count(), 2)
 
 
 class ConcurrentTransferTestCase(WalletTestCase):
 
     def test_transfer(self):
-        """We're going to simulate concurrent transfer to a
+        """Simulating concurrent transfer to a
         single wallet."""
         INITIAL_BALANCE = 200
         self._create_initial_balance(INITIAL_BALANCE)
@@ -139,9 +139,9 @@ class ConcurrentTransferTestCase(WalletTestCase):
                 wallet2 = Wallet.objects.select_for_update().get(pk=wallet2_id)
                 wallet.transfer(wallet2, TRANSFER)
 
-                # We simulate a long transaction so that
-                # when the other thread comes in, this
-                # thread still holds the lock.
+                 """Simulate a long transaction so that
+                 when the other thread comes in, this
+                 thread still holds the lock."""
                 time.sleep(1)
 
         t1 = threading.Thread(target=transfer_thread)
@@ -154,8 +154,8 @@ class ConcurrentTransferTestCase(WalletTestCase):
         wallet = Wallet.objects.get(pk=self.wallet.id)
         wallet2 = Wallet.objects.get(pk=wallet2_id)
 
-        # Assert that all transfers were made and that
-        # both applies to the wallet's current_balance.
+        """ Asserting that all transfers were made and that
+        both applies to the wallet's current_balance."""
         self.assertEqual(wallet.current_balance,
                 INITIAL_BALANCE - (TRANSFER * 2))
         self.assertEqual(wallet2.current_balance, TRANSFER * 2)
